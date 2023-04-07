cb1f20d560ca8c13ccc82a9f5c3cccb25b18e1c3
cosyrusty <priyaanyadav@gmail.com>
2023-04-07 18:37:10 +0530

    linear search

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# algorithms
+# Algorithms
 Implementation of different kinds of Algorithms and Data Structures.
+
+* Algorithm is a set of instruction for accomplishing a task.
+* All programs can be constructed from just three structures:
+    * sequence, selection, and iteration.
---
+++ src/searching/linear_search.rs
@@ -0,0 +1,50 @@
+pub fn linear_search<T>(value: &T, arr: &[T]) -> Option<usize>
+where
+    T: PartialEq,
+{
+    for (i, v) in arr.iter().enumerate() {
+        if v == value {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_strings() {
+        let index = linear_search(&"a", &["a", "b", "c", "d", "google", "zoo"]);
+        assert_eq!(index, Some(0));
+    }
+
+    #[test]
+    fn search_ints() {
+        let index = linear_search(&4, &[1, 2, 3, 4]);
+        assert_eq!(index, Some(3));
+
+        let index = linear_search(&3, &[1, 2, 3, 4]);
+        assert_eq!(index, Some(2));
+
+        let index = linear_search(&2, &[1, 2, 3, 4]);
+        assert_eq!(index, Some(1));
+
+        let index = linear_search(&1, &[1, 2, 3, 4]);
+        assert_eq!(index, Some(0));
+    }
+
+    #[test]
+    fn not_found() {
+        let index = linear_search(&5, &[1, 2, 3, 4]);
+        assert_eq!(index, None);
+    }
+
+    #[test]
+    fn empty() {
+        let index = linear_search(&1, &[]);
+        assert_eq!(index, None);
+    }
+}
